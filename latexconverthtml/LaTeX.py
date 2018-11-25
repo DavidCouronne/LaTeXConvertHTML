6a16e72432875fdf9fd049e9b3f21939eec24406
DavidCouronne <couronne.david@gmail.com>
2018-11-25 10:51:21 +0100

    ajout pstricks
    Détection des environnements pstricks

--- latexconverthtml/LaTeX.py
+++ latexconverthtml/LaTeX.py
@@ -1,31 +1,43 @@
 #!/usr/local/bin/python
 # -*- coding:utf-8 -*-
-#Auteur: David Couronné
-#Convertion automatique de LaTeX en HTML
+# Auteur: David Couronné
+# Convertion automatique de LaTeX en HTML
 
 import re
+
+from latexconverthtml import LaTeXCommands, setup
 
 
 class Source:
     def __init__(self, original=""):
-        self.original = original #On garde l'original pour développement
+        self.original = original  # On garde l'original pour développement
         self.contenu = original
         self.lines = self.contenu.splitlines()
+        
 
     def collapseLines(self):
         """Recolle les lignes dans self.contenu"""
         self.contenu = "\n".join(self.lines)
 
     def cleanSpace(self):
-        """Enlève les espaces en début et fin de chaque ligne"""
+        """Agit sur les lignes.
+        Enlève les espaces en début et fin de chaque ligne"""
         new_lines = []
         for line in self.lines:
             line = line.strip()
             new_lines.append(line)
         self.lines = new_lines
 
+    def cleanCommand(self):
+        """Agit sur le contenu.
+        Suprrime toutes les commandes du fichier setup.py"""
+        for command in setup.listeCommandesClean:
+            self.contenu = re.sub(command.regex, "", self.contenu)
+            self.lines = self.contenu.splitlines()
+
     def convertEnumerate(self):
-        """Converti les environnements enumerate en listes html"""
+        """Agit sur les lignes.
+        Converti les environnements enumerate en listes html"""
         level_enumerate = 0
         level_item = 0
         new_lines = []
@@ -50,76 +62,22 @@ class Source:
             new_lines.append(line)
         self.lines = new_lines
 
-
-class LaTeXCommand:
-    def __init__(self, nom, arg=1, optn=False):
-        self.nom = nom
-        self.arg = arg
-        self.optn = optn
-        if arg == 0:
-            self.regex = re.compile(self.nom + r"\b")
-        if arg == 1:
-            self.regex = re.compile(self.nom + r"\{([\d|\w|\.|-]*)\}")
-
-    def find(self, contenu):
-        return contenu.find(self.nom)
-
-    def findCommand(self, texte):
-        index = texte.find(self.nom)
-        avant = texte[:index]
-        apres = texte[index+len(self.nom):]
-        argOptn = ""
-        listeArg = []
-        if self.optn:
-            if apres[0] == "[":
-                i = 0
-                while apres[i] != "]":
-                    i = i + 1
-                argOptn = apres[1:i]
-                apres = apres[i+2:]
+    def findPstricks(self):
+        """Agit sur les lignes.
+        Essaie de trouver les envir
+        onnements Pstricks"""
+        in_pstricks = False
+        lignes_pstricks = []
+        pstricks = []
+        for line in self.lines:
+            if in_pstricks:
+                lignes_pstricks.append(line)
+                if r"\end{pspicture}" in line:
+                    in_pstricks = False
+                    pstricks.append("\n".join(lignes_pstricks))
+                    lignes_pstricks = []
             else:
-                apres = apres[1:]
-        else:
-            apres = apres[1:]
-        argRest = self.arg
-        avantManip = "{"+apres
-        while argRest != 0:
-            argRest = argRest - 1
-            ouvert2 = apres.find("{")
-            ferme1 = apres.find("}")
-            if ouvert2 == -1:
-                ouvert2 = ferme1
-            while ouvert2 < ferme1:
-                ouvert2 = apres.find("{", ferme1+1)
-                ferme1 = apres.find("}", ferme1+1)
-            listeArg.append(apres[:ferme1])
-            apres = apres[ferme1+2:]
-        longueurArgument = 0
-        for argument in listeArg:
-            longueurArgument = longueurArgument + len(argument)+2
-
-        apres = avantManip[longueurArgument:]
-        return index, avant, apres, argOptn, listeArg
-
-    def cleanCommand(self, contenu):
-        passe = 0
-        while self.nom in contenu:
-            passe = passe + 1
-            index, avant, apres, argOptn, listeArg = self.findCommand(contenu)
-            # print("Passe"+str(passe),apres)
-            contenu = avant+apres
-        return contenu
-
-    def replaceCommand(self, contenu, listeReplace):
-        while self.nom in contenu:
-            index, avant, apres, argOptn, listeArg = self.findCommand(contenu)
-            texte = ""
-            for argument in listeReplace:
-                if type(argument) is type("c"):
-                    texte = texte + argument
-                elif argument == 0:
-                    texte = texte + "[" + argOptn + "]"
-                else:
-                    texte = texte + "{" + listeArg[argument-1] + "}"
-            contenu = avant + texte + apres
-        return contenu
+                if r"\psset" in line or r"\begin{pspicture}" in line:
+                    in_pstricks = True
+                    lignes_pstricks.append(line)
+        self.pstricks = pstricks
--- latexconverthtml/setup.py
+++ latexconverthtml/setup.py
@@ -1,16 +1,23 @@
 #!/usr/local/bin/python
 # -*- coding:utf-8 -*-
-#Auteur: David Couronné
-#Convertion automatique de LaTeX en HTML
-from latexconverthtml import LaTeX
+# Auteur: David Couronné
+# Convertion automatique de LaTeX en HTML
 import re
 
-listeCommandesClean = [LaTeX.LaTeXCommand(r"\\newpage",0),
-    LaTeX.LaTeXCommand(r"\\hfill",0),
-    LaTeX.LaTeXCommand(r"\\medskip",0),
-    LaTeX.LaTeXCommand(r"\\vspace",1)]
-re_vspace = r"\\vspace\{([\d|\w|\.|-]*)\}"
-re_hfill = r"\b\\hfill\b"
-re_newpage = r"\\newpage\b"
+from latexconverthtml import LaTeXCommands
 
-newpage = LaTeX.LaTeXCommand(r"\\newpage",0)
+# Commandes à supprimer
+listeCommandesClean = [LaTeXCommands.LaTeXCommand(r"\\newpage", 0),
+                       LaTeXCommands.LaTeXCommand(r"\\hfill", 0),
+                       LaTeXCommands.LaTeXCommand(r"\\medskip", 0),
+                       LaTeXCommands.LaTeXCommand(r"\\vspace", 1),
+                       LaTeXCommands.LaTeXCommand(r"\\bigskip", 0),
+                       LaTeXCommands.LaTeXCommand(r"\\smallskip", 0),
+                       LaTeXCommands.LaTeXCommand(r"\\hspace", 1),
+                       LaTeXCommands.LaTeXCommand(r"\\setlength", 0),
+                       LaTeXCommands.LaTeXCommand(r"\\parindent", 1),
+                       LaTeXCommands.LaTeXCommand(r"\\pagestyle", 1),
+                       LaTeXCommands.LaTeXCommand(r"\\rhead", 1),
+                       LaTeXCommands.LaTeXCommand(r"\\Large", 0),
+                       LaTeXCommands.LaTeXCommand(r"\\thispagestyle", 1)
+                       ]
